b74ca25f05ace1c9b8323c9a9ce74ef89d8d20ce
Laurent Enzo <enzo.laurent@epitech.eu>
2022-05-08 20:49:41 +0200

    ADD: compressors functions

--- src/ImageCompressor.hs
+++ src/ImageCompressor.hs
@@ -3,9 +3,11 @@ module ImageCompressor where
 import System.Environment
 import System.Exit (exitWith, ExitCode (ExitSuccess, ExitFailure), exitSuccess)
 import Text.Read
+import Data.List
 import Control.Monad
 import Control.Exception
 import Data.Char
+import GHC.Float
 
 data Params =
     Params { colors :: Maybe Int
@@ -31,7 +33,16 @@ data Pixel =
             }
 instance Show Pixel where
     show (Pixel (x, y) (r, g, b)) = "(" ++ show x ++ "," ++ show y ++ ") (" 
-                    ++ show r ++ "," ++ show g ++ "," ++ show b ++ ")" ++ "\n"
+                    ++ show r ++ "," ++ show g ++ "," ++ show b ++ ")"
+
+data Cluster =
+    Cluster { new :: Color
+             , old :: Color
+             , pixels :: [Pixel]
+            }
+instance Show Cluster where
+    show (Cluster (r, g, b) _ _) = "--\n(" ++ show r ++ "," ++ show g ++
+                                        "," ++ show b ++ ")\n-"
 
 initConf :: Params
 initConf = Params { colors = Nothing
@@ -43,11 +54,13 @@ toConfig :: Params -> Maybe Config
 toConfig (Params Nothing _ _) = Nothing
 toConfig (Params _ Nothing _) = Nothing
 toConfig (Params _ _ Nothing) = Nothing
-toConfig (Params (Just colors) (Just converge) (Just filepath)) = do
-    return $ Config { configColors = colors
-                    , configConverge = converge
-                    , configFilepath = filepath
-                    }
+toConfig (Params (Just colors) (Just converge) (Just filepath))
+        | colors <= 0 || converge <= 0 = Nothing
+        | otherwise = do
+                return $ Config { configColors = colors
+                                , configConverge = converge
+                                , configFilepath = filepath
+                                }
 
 manageArgs :: [String] -> Maybe Params
 manageArgs args = go initConf args
@@ -63,25 +76,48 @@ manageArgs args = go initConf args
                             go config{filepath = Just valueFilepath} params
         go _ _ = Nothing
 
-distance :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+distance :: Color -> Color -> Float
 distance (x, xs, xt) (y, ys, yt) =
-    sqrt ((x - y)**2 + (xs - ys)**2 + (xt - yt)**2)
+    sqrt ((int2Float x - int2Float y)**2 + (int2Float xs - int2Float ys)**2 +
+        (int2Float xt - int2Float yt)**2)
 
 manageCompressor :: Config -> IO()
-manageCompressor conf@(Config n l f) = openFile n f
+manageCompressor conf@(Config n l f) = openFile n l f
 
 printException :: SomeException -> IO String
 printException _ = putStrLn "File not found." >>
                    exitWith (ExitFailure 84)
 
-openFile :: Int -> String -> IO ()
-openFile n f = do
+checkFile :: String -> IO ()
+checkFile [] = return ()
+checkFile (x:xs) | isDigit x == False && x /= ')' && x /= '(' && x /= ' ' &&
+                            x /= ',' && x /= '\n' = exitWith (ExitFailure 84)
+                 | otherwise = checkFile xs
+
+checkPixels :: [Pixel] -> IO ()
+checkPixels [] = return ()
+checkPixels ((Pixel (x, y) (r, g, b)):pxls) 
+                | r > 255 || g > 255 || b > 255 = exitWith (ExitFailure 84)
+                | otherwise = checkPixels pxls
+
+openFile :: Int -> Float -> String -> IO ()
+openFile n l f = do
     file <- catch (readFile f) printException
-    let pixels = fct [] (lines file)
-    print pixels
-    -- let cluster =  (pixelsToColors(getCluster pixel [] n g))
-    -- let index = (getClustersIndexs pixel (cluster))
-    -- clusterLoop n pixel index cluster
+    checkFile file
+    let pixels = getPixels [] (lines file)
+    checkPixels pixels
+    let clusters = createCluster [] pixels n
+    let compressed = compress (changeCluster pixels clusters) [] pixels l
+    displayClusters compressed
+
+displayClusters :: [Cluster] -> IO ()
+displayClusters [] = return ()
+displayClusters (x@(Cluster _ _ pxls):xs) = print x >> displayPixels pxls >>
+                                            displayClusters xs
+
+displayPixels :: [Pixel] -> IO ()
+displayPixels [] = return ()
+displayPixels (x:xs) = print x >> displayPixels xs
 
 myGetNbr :: String -> Int -> Int
 myGetNbr [] nb = nb
@@ -113,8 +149,59 @@ parseLine pxl@(Pixel p@(x, y) c@(r, g, b)) (l:ls) | l == '(' &&
                                                   | l == ',' &&
     (countOccurences ls '(' 0) == 0 && (countOccurences ls ',' 0) == 0 =
         parseLine (Pixel p (r, g, myGetNbr ls 0)) ls
-                                                  | otherwise = parseLine pxl ls
+                                                | otherwise = parseLine pxl ls
 
-fct :: [Pixel] -> [String] -> [Pixel]
-fct pixels [] = pixels
-fct pixels (x:y) = fct (reverse ((parseLine initPixel x):reverse pixels)) y
+getPixels :: [Pixel] -> [String] -> [Pixel]
+getPixels pixels [] = pixels
+getPixels pixels (x:y) = getPixels
+                (reverse ((parseLine initPixel x):reverse pixels)) y
+
+createCluster :: [Cluster] -> [Pixel] -> Int -> [Cluster]
+createCluster list [] _ = list
+createCluster list _ 0 = list
+createCluster list (pixel@(Pixel _ col):xs) l | checkColor col list =
+                                                createCluster list xs l
+    | otherwise = createCluster (Cluster col col [pixel]:list) xs (l - 1)
+
+checkColor :: Color -> [Cluster] -> Bool
+checkColor _ [] = False
+checkColor color (Cluster c _ _:x) | color == c = True
+    | otherwise = checkColor color x
+
+compress :: [Cluster] -> [Cluster] -> [Pixel] -> Float -> [Cluster]
+compress oclusters [] pxls l = compress oclusters
+                            (changeCluster pxls oclusters) pxls l
+compress oclusters nclusters pxls l | checkConvergence l nclusters = oclusters
+                          | otherwise = compress nclusters [] pxls l
+
+changeCluster :: [Pixel] -> [Cluster] -> [Cluster]
+changeCluster pixels clusters = foldl
+                checkPixelsForCluster (map changeClusterColor clusters) pixels
+
+checkPixelsForCluster :: [Cluster] -> Pixel -> [Cluster]
+checkPixelsForCluster cluster pixel = putPixelInCluster pixel
+                    (sortOn (getPixelAndClusterDistance pixel) cluster)
+
+putPixelInCluster :: Pixel -> [Cluster] -> [Cluster]
+putPixelInCluster _ [] = []
+putPixelInCluster pixel ((Cluster old new clpxls):rest) =
+                    ((Cluster old new (pixel:clpxls)):rest)
+
+getPixelAndClusterDistance :: Pixel -> Cluster -> Float
+getPixelAndClusterDistance (Pixel _ pixelColor) (Cluster oldClColor _ _) =
+                                            distance pixelColor oldClColor
+
+changeClusterColor :: Cluster -> Cluster
+changeClusterColor (Cluster old _ pixels) = (Cluster (getAverageColor
+        (foldl mergeColor (0, 0, 0) pixels) (length pixels)) old [])
+
+mergeColor :: Color -> Pixel -> Color
+mergeColor (r, g, b) (Pixel _ (rs, gs, bs)) = (r + rs, g + gs, b + bs)
+
+getAverageColor :: Color -> Int -> Color
+getAverageColor (r, g, b) l = ((r `div` l), (g `div` l), (b `div` l))
+
+checkConvergence :: Float -> [Cluster] -> Bool
+checkConvergence _ [] = True
+checkConvergence l (Cluster ocol ncol _:xs) | distance ocol ncol > l = False
+                                        | otherwise = checkConvergence l xs
